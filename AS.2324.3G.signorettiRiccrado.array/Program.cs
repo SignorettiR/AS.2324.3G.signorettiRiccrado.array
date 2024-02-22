8a83e608a2b01ee6689cfbafc80cb34a9882bc92
SignorettiR <ricky.signoretti@gmail.com>
2024-02-22 09:10:06 +0100

    “carica vettori”

--- AS.2324.3G.signorettiRiccrado.array/Program.cs
+++ AS.2324.3G.signorettiRiccrado.array/Program.cs
@@ -3,7 +3,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-    }
-}
 
+        double[] pesi = new double[100];
+        int[] eta = new int[100];
+        int numPersone;
+
+        Console.Write("Inserisci il numero di persone da caricare: ");
+        numPersone = int.Parse(Console.ReadLine());
+
+        CaricaVettori(ref pesi, ref eta, numPersone);
+
+    }
+    static void CaricaVettori(ref double[] pesi, ref int[] eta, int numPersone)
+    {
+        Random rnd = new Random();
+
+        for (int i = 0; i < numPersone; i++)
+        {
+            pesi[i] = rnd.Next(50, 101);
+            eta[i] = rnd.Next(18, 100);
+        }
+    }
+
+}
